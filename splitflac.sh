b012da490a57f10a093d2d2596aa9cbdf8352629
Andrii Lytvyn <lytvyn349@gmail.com>
2022-02-23 19:50:53 +0200

    Update convertaudio and splitflac.sh

---
+++ splitflac.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# author: Andrii Lytvyn (lytvyn349@gmail.com)
+# description: split album flac into tracks and tag them using cuesheet
+# required: cuetools; shntool;
+
+# $1 - cuefile or flac file path (provided that filenames are equal)
+
+split()
+{
+    filename="$(basename "$1")"
+
+    shnsplit -f "$filename"."cue" -t "%n. %t" -o flac "$filename"."flac"
+    cuetag.sh "$filename"."cue" [0-9]*.flac
+}
+
+split "$1"
